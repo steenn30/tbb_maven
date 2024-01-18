514b5a61c7c8c0c15aa0439bdd68e30e6994885b
steenn30 <steenn30@gmail.com>
2024-01-18 14:56:56 -0500

    test

--- src/test/java/com/tbb/TbbApplicationTests.java
+++ src/test/java/com/tbb/TbbApplicationTests.java
@@ -1,13 +1,21 @@
 package com.tbb;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TbbApplicationTests {
 
+	public static final Logger logger = LoggerFactory.getLogger(TbbApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("this is test case");
+		assertEquals(true,true);
+	
 	}
 
 }
